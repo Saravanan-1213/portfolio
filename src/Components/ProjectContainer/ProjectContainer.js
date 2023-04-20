c7641413842073e4ba0851c0240d56c806aa9651
Saravanan-1213mm <saravanansekar033@gmail.com>
2023-04-20 14:21:48 +0530

    Project Updated

--- src/Components/ProjectContainer/ProjectContainer.js
+++ src/Components/ProjectContainer/ProjectContainer.js
@@ -14,15 +14,22 @@ const ProjectContainer = () => {
     {
       img: "https://www.shutterstock.com/image-vector/online-cinema-art-movie-watching-260nw-586719869.jpg",
       title: "Movie APP",
-      desc: "This Movie App is created using ReactJs",
+      desc: "This Movie App is created using ReactJs, NodeJS, MongoDB",
       link: "https://cerulean-pudding-4c685a.netlify.app",
+    },
+    {
+      img: "https://st3.depositphotos.com/3259079/35899/v/600/depositphotos_358995556-stock-illustration-colleagues-talk-each-other-computer.jpg",
+      title: "Zoom Clone",
+      desc: "Zoom Clone is created using ReactJs, NodeJS",
+      link: "https://peppy-buttercream-ed733d.netlify.app",
     },
   ];
   return (
     <Element className="projectContainer" id="projects">
       <h1>Projects</h1>
       <p>
-        Here some projects which I done using HTMl, CSS, JAVASCRIPT, REACTJS.
+        Here some projects which I done using HTMl, CSS, JAVASCRIPT, REACTJS,
+        NODEJS, EXPRESS, MONGODB.
       </p>
       <div className="projectContainer__projects">
         {projects.map((project, index) => {
